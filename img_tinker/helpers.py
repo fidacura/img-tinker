9ea9b113bfadadfbed3cadbee5fd44d93dff94d8
fidacura <hello@fidacura.xyz>
2024-06-20 16:50:06 +0100

    feat(logger): add helper function; a bsic logger that outputs the process to the console.

--- img_tinker/helpers.py
+++ img_tinker/helpers.py
@@ -1,11 +1,31 @@
-# import logging
+import logging
 
-# def setup_logger(name, level=logging.INFO):
-#     """
-#     Sets up a basic logger that outputs to the console.
-#     Args:
-#         name (str): Name of the logger.
-#         level: Logging level, e.g., logging.INFO, logging.DEBUG
-#     Returns:
-#         A configured logger instance.
-#     """
+def setup_logger(name, level=logging.INFO):
+    """
+    A basic logger that outputs to the console.
+    
+    Args:
+        name (str): Name of the logger.
+        level: Logging level, e.g. logging.INFO, logging.DEBUG
+        
+    Returns:
+        A configured logger instance.
+    """
+    # Create a logger
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    # Create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+
+    # Create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Add formatter to ch
+    ch.setFormatter(formatter)
+
+    # Add ch to logger
+    logger.addHandler(ch)
+
+    return logger
